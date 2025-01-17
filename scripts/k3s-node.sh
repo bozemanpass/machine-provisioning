40e17264534007179f63e4699f720ed7b09e4d48
Thomas E Lackey <telackey@bozemanpass.com>
2025-01-16 18:10:49 -0600

    Install nginx and cert-manager by default. (#1)
    Configure nginx ingress and cert-manager by default.

--- scripts/k3s-node.sh
+++ scripts/k3s-node.sh
@@ -1,15 +1,50 @@
- #!/usr/bin/env bash
+#!/usr/bin/env bash
 if [[ -n "$BPI_SCRIPT_DEBUG" ]]; then
     set -x
 fi
 
 install_dir=~/bin
 
+NEEDS_WARN=true
+LETSENCRYPT_EMAIL=""
+DO_TOKEN=""
+
+while getopts "ye:t:" arg; do
+  case $arg in
+    y)
+      NEEDS_WARN=false
+      ;;
+    e)
+      LETSENCRYPT_EMAIL=$OPTARG
+      ;;
+    t)
+      DO_TOKEN=$(echo -n "$OPTARG" | base64 -w0)
+      ;;
+  esac
+done
+
+function retry {
+  local try=0
+  local max=5
+  local delay=10
+  while [ $try -lt $max ]; do
+    try=$((try + 1))
+    echo "Try $try of $* ..."
+    $* && RC=$? || RC=$?
+    if [ $RC -eq 0 ]; then
+      return 0
+    else
+      sleep $delay
+    fi
+  done
+  return 1
+}
+
 # Skip the package install stuff if so directed
 if ! [[ -n "$BPI_INSTALL_SKIP_PACKAGES" ]]; then
 
 # First display a reasonable warning to the user unless run with -y
-if ! [[ $# -eq 1 && $1 == "-y" ]]; then
+if [[ "$NEEDS_WARN" == "true" ]]; then
   echo "**************************************************************************************"
   echo "This script requires sudo privilege. It installs utilities"
   echo "into: ${install_dir}. It also *removes* any existing docker installed on"
@@ -91,8 +126,164 @@ echo "Installing k3s"
 k3s_installer_file=$HOME/install-k3s.sh
 curl -sfL https://get.k3s.io -o ${k3s_installer_file}
 chmod +x ${k3s_installer_file}
-sudo ${k3s_installer_file}
+
+export INSTALL_K3S_EXEC="--disable=traefik"
+sudo --preserve-env=INSTALL_K3S_EXEC ${k3s_installer_file}
 echo "Installed k3s"
+
+echo "**************************************************************************************"
+echo "Installing nginx ingress"
+sudo kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.12.0/deploy/static/provider/baremetal/deploy.yaml
+
+cat > /tmp/lb.yml.$$ <<EOF
+apiVersion: v1
+kind: Service
+metadata:
+  name: ingress-nginx-controller-loadbalancer
+  namespace: ingress-nginx
+spec:
+  selector:
+    app.kubernetes.io/component: controller
+    app.kubernetes.io/instance: ingress-nginx
+    app.kubernetes.io/name: ingress-nginx
+  ports:
+    - name: http
+      port: 80
+      protocol: TCP
+      targetPort: 80
+    - name: https
+      port: 443
+      protocol: TCP
+      targetPort: 443
+  type: LoadBalancer
+EOF
+
+sudo kubectl apply -f /tmp/lb.yml.$$
+rm -f /tmp/lb.yml.$$
+
+sudo kubectl annotate ingressclass nginx ingressclass.kubernetes.io/is-default-class=true
+
+echo "Installed nginx ingress"
+
+echo "**************************************************************************************"
+echo "Installing cert-manager"
+
+sudo kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.16.2/cert-manager.yaml
+
+
+TRY=0
+MAX_TRIES=30
+CM_UP="false"
+while [ "$CM_UP" == "false" ]; do
+  TRY=$((TRY + 1))
+  echo "Checking if cert-manager is up (try $TRY)..."
+  if [[ `sudo kubectl get pods --namespace cert-manager | grep Running | wc -l` -ge 3 ]]; then
+    CM_UP="true"
+    echo "cert-manager is up!"
+  elif [[ $TRY -ge $MAX_TRIES ]]; then
+    echo "ERROR: cert-manager failed to come up." 1>&2
+    exit 1
+  else
+    sleep 10
+  fi
+done
+
+cat > $HOME/letsencrypt-prod.yml <<EOF
+apiVersion: cert-manager.io/v1
+kind: ClusterIssuer
+metadata:
+  name: letsencrypt-prod
+  namespace: cert-manager
+spec:
+  acme:
+    # The ACME server URL
+    server: https://acme-v02.api.letsencrypt.org/directory
+    # Email address used for ACME registration
+    email: $LETSENCRYPT_EMAIL
+    # Name of a secret used to store the ACME account private key
+    privateKeySecretRef:
+      name: letsencrypt-prod
+    # Enable the HTTP-01 challenge provider
+    solvers:
+    - http01:
+        ingress:
+          class: nginx
+EOF
+
+cat > $HOME/letsencrypt-stage.yml <<EOF
+apiVersion: cert-manager.io/v1
+kind: ClusterIssuer
+metadata:
+ name: letsencrypt-staging
+ namespace: cert-manager
+spec:
+ acme:
+   # The ACME server URL
+   server: https://acme-staging-v02.api.letsencrypt.org/directory
+   # Email address used for ACME registration
+   email: $LETSENCRYPT_EMAIL
+   # Name of a secret used to store the ACME account private key
+   privateKeySecretRef:
+     name: letsencrypt-staging
+   # Enable the HTTP-01 challenge provider
+   solvers:
+   - http01:
+       ingress:
+         class:  nginx
+EOF
+
+cat > $HOME/digitalocean-dns.yml <<EOF
+apiVersion: v1
+data:
+  access-token: $DO_TOKEN
+kind: Secret
+metadata:
+  name: digitalocean-dns
+  namespace: cert-manager
+EOF
+
+
+cat > $HOME/letsencrypt-prod-dns01.yml <<EOF
+apiVersion: cert-manager.io/v1
+kind: ClusterIssuer
+metadata:
+  name: letsencrypt-prod-dns
+  namespace: cert-manager
+spec:
+  acme:
+    # Email address used for ACME registration
+    email: $LETSENCRYPT_EMAIL
+    server: https://acme-v02.api.letsencrypt.org/directory
+    privateKeySecretRef:
+      # Name of a secret used to store the ACME account private key
+      name: letsencrypt-prod
+    solvers:
+      - dns01:
+          digitalocean:
+            tokenSecretRef:
+              name: digitalocean-dns
+              key: access-token
+EOF
+
+if [[ ! -z "$LETSENCRYPT_EMAIL" ]]; then
+  echo "Adding letsencrypt-prod ClusterIssuer..."
+  retry sudo kubectl apply -f $HOME/letsencrypt-prod.yml
+  echo "Adding letsencrypt-staging ClusterIssuer..."
+  retry sudo kubectl apply -f $HOME/letsencrypt-stage.yml
+  if [[ ! -z "$DO_TOKEN" ]]; then
+    echo "Adding digitalocean-dns Secret..."
+    retry sudo kubectl apply -f $HOME/digitalocean-dns.yml
+    echo "Adding letsencrypt-prod-dns ClusterIssuer..."
+    retry sudo kubectl apply -f $HOME/letsencrypt-prod-dns01.yml
+  else
+    echo "No DigitalOcean access token specified, so a DNS-based ClusterIssuer's could not be created.  Template files created at $HOME/digitalocean-dns.yml and $HOME/letsencrypt-prod-dns01.yml"
+  fi
+else
+  echo "No e-mail specified, so ClusterIssuer's could not be created.  Template files created at $HOME/letsencrypt-prod.yml and $HOME/letsencrypt-stage.yml"
+fi
+
+echo "Installed cert-manager"
+
 
 # End of long if block: Skip the package install stuff if so directed
 fi
