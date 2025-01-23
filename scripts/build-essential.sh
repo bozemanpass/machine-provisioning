759cdfaf9602f2b77f00a6d78deb8db1e29bd0f8
Thomas E Lackey <telackey@bozemanpass.com>
2025-01-22 19:16:54 -0600

    build-essential

---
+++ scripts/build-essential.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+if [[ -n "$BPI_SCRIPT_DEBUG" ]]; then
+    set -x
+fi
+
+export DEBIAN_FRONTEND=noninteractive
+export NEEDRESTART_MODE=a
+
+APT_INSTALL="sudo --preserve-env=DEBIAN_FRONTEND,NEEDRESTART_MODE apt -y install"
+
+set -eo pipefail  ## https://vaneyckt.io/posts/safer_bash_scripts_with_set_euxo_pipefail/
+
+sudo apt update
+$APT_INSTALL build-essential
