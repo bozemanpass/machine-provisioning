b4f5ea435e1733e5cdc3cc8b1fa1cd3e9d8b4a09
Thomas E Lackey <telackey@bozemanpass.com>
2025-01-23 00:52:43 +0000

    Check for existing

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -8,7 +8,7 @@ export NEEDRESTART_MODE=a
 
 APT_INSTALL="sudo --preserve-env=DEBIAN_FRONTEND,NEEDRESTART_MODE apt -y install"
 
-set -euo pipefail  ## https://vaneyckt.io/posts/safer_bash_scripts_with_set_euxo_pipefail/
+set -eo pipefail  ## https://vaneyckt.io/posts/safer_bash_scripts_with_set_euxo_pipefail/
 
 which docker >/dev/null && rc=$? || rc=$?
 if [[ $rc -eq 0 ]]; then
